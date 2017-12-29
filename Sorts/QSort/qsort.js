8cc4869c5cf67966b6b763b35b82f1d45d59a334
kirillherz <kirill.herz@gmail.com>
2017-12-29 13:59:01 +0300

    Внесены изменения в структуру папок
    Папки с реализациями сортировок помещены в общую папку Sorts. Добавлен файл swap.js

--- Sorts/QSort/qsort.js
+++ Sorts/QSort/qsort.js
@@ -1,8 +1,3 @@
-function swap(arr, a, b) {
-    var temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-}
 function partition(arr, start, end) {
     var pivot = arr[Math.floor(Math.random() * (end - start + 1)) + start];
     var l = start;
---
+++ Sorts/swap.js
@@ -0,0 +1,5 @@
+function swap(arr,a,b){
+    arr[a] = arr[a] - arr[b];
+    arr[b] = arr[a];
+    arr[a] = arr[b] - arr[a];
+}
