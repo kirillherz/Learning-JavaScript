9d3a5d14d438edb50cd6cddbb1db4bdf1c1a4d33
kirillherz <kirill.herz@gmail.com>
2017-12-30 18:13:20 +0300

    Написана функция unshift

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -5,25 +5,28 @@ function List() {
     var _head = null;
     var DATA = 0;
     var NEXT = 1;
+    var _getEmptyCell = function () {
+        var index;
+        if (_stack.getSize() == 0) {
+            _items.push([null, null]);
+            index = _items.length - 1;
+        } else {
+            index = _stack.pop();
+        }
+        return index;
+    };
     this.push = function (x) {
         if (_size == 0) {
             _items.push([x, null]);
-            _size += 1;
             _head = 0;
         } else {
-            if (_stack.getSize() == 0) {
-                _items.push([x, _head]);
-                _head = _items.length - 1;
-                _size += 1;
-            } else {
-                var temp = _stack.pop();
-                _items[_head][NEXT] = temp;
-                _head = temp;
-                _items[_head][DATA] = x;
-                _items[_head][NEXT] = null;
-                _size += 1;
-            }
+            var temp = _getEmptyCell();
+            _items[_head][NEXT] = temp;
+            _head = temp;
+            _items[_head][DATA] = x;
+            _items[_head][NEXT] = null;
         }
+        _size += 1;
     };
     this.pop = function () {
         var x = _items[_head][DATA];
@@ -31,10 +34,23 @@ function List() {
         _head = _items[_head][NEXT];
         if (tempHead == _items.length - 1) {
             _items.pop();
-        }else{
+        } else {
+            _items[tempHead][null];
+            _items[tempHead][null];
             _stack.push(tempHead);
         }
         _size -= 1;
         return x;
+    };
+    this.unshift = function (x) {
+        var _temp = _head;
+        while (_items[_temp][NEXT] != null) {
+            _temp = _items[_temp][NEXT];
+        }
+        var index = _getEmptyCell();
+        _items[index][DATA] = x;
+        _items[index][NEXT] = null;
+        _items[_temp][NEXT] = index;
+
     };
 }
