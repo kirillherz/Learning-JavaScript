8ff18dadc01b866f245de90889234a6df6b3485d
kirillherz <kirill.herz@gmail.com>
2017-12-30 17:16:12 +0300

    Создан класс List и добавлена функция push

---
+++ Data Structures/LinkedList/LinkedList.js
@@ -0,0 +1,26 @@
+function List() {
+    var _size = 0;
+    var _items = [];
+    var _stack = new Stack();
+    var _head = null;
+    this.push= function (x) {
+        if (_size == 0) {
+            _items.push([x, null]);
+            _size += 1;
+            _head = 0;
+        } else {
+            if (_stack.getSize() == 0) {
+                _items.push([x, _head]);
+                _head = _items.length - 1;
+                _size += 1;
+            } else {
+                var temp = _stack.pop();
+                _items[_head][1] = temp;
+                _head = temp;
+                _items[_head][0] = x;
+                _items[_head][1] = null;
+                _size += 1;
+            }
+        }
+    }
+}
