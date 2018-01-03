1f7f989307ebbd27fb02bbadfbeca72c6ae4505e
kirillherz <kirill.herz@gmail.com>
2018-01-03 19:44:41 +0300

    Выполнено форматирование

--- Data Structures/XorList/XorList.js
+++ Data Structures/XorList/XorList.js
@@ -1,4 +1,4 @@
-function XorList(){
+function XorList() {
     var _items = [];
     var _size = 0;
     var _head = null;
@@ -7,12 +7,12 @@ function XorList(){
     var DATA = 0;
     var NEXT = 1;
     var _stack = [];
-    var _getEmptyCell = function(){
+    var _getEmptyCell = function () {
         var index;
-        if(_stack.length === 0){
-            _items.push([null,null]);
+        if (_stack.length === 0) {
+            _items.push([null, null]);
             index = _items.length - 1;
-        } else{
+        } else {
             index = _stack.pop();
         }
         return index;
