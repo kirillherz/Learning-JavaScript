23e0eab60af428b80f9bfd5a594e44f71bd393b6
kirillherz <kirill.herz@gmail.com>
2018-01-03 19:18:39 +0300

    Создан класс XorList
    Создан класс XorList и добавлен метод _getEmptyCell

---
+++ Data Structures/XorList/XorList.js
@@ -0,0 +1,20 @@
+function XorList(){
+    var _items = [];
+    var _size = 0;
+    var _head = null;
+    var _tail = null;
+    var NULL = -1;
+    var DATA = 0;
+    var NEXT = 1;
+    var _stack = [];
+    this._getEmptyCell = function(){
+        var index;
+        if(_stack.length === 0){
+            _items.push([null,null]);
+            index = _items.length - 1;
+        } else{
+            index = _stack.pop();
+        }
+        return index;
+    };
+}
