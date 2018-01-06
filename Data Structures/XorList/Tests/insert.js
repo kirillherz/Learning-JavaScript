802c5a8e7621c35229a3b2df6d0ed43ab60bd21c
kirillherz <kirill.herz@gmail.com>
2018-01-06 16:29:34 +0300

    Добавлен автоматический тест для метода insert
    Проверяет правильность подсчета адресов

--- Data Structures/XorList/Tests/insert.js
+++ Data Structures/XorList/Tests/insert.js
@@ -6,4 +6,20 @@ describe("insert", function () {
         l.insert(2, 2);
         assert.equal(l.getSize(), 3);
     });
+    it("Правильно считает адреса", function () {
+        var l = new XorList();
+        l.insert(0, 0);
+        l.insert(1, 1);
+        l.insert(2, 2);
+        l.insert(3, 3);
+        l.insert(4, 4);
+        l.insert(2, 2);
+        var iterator = l.getAdressIterator();
+        assert.equal(iterator.next().value, -2);
+        assert.equal(iterator.next().value, 5);
+        assert.equal(iterator.next().value, 3);
+        assert.equal(iterator.next().value, 6);
+        assert.equal(iterator.next().value, 6);
+        assert.equal(iterator.next().value, -4);
+    });
 });
