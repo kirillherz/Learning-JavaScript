a0b5f9821edbc6947d1707c6a4f69b75e9b7660b
kirillherz <kirill.herz@gmail.com>
2018-01-07 18:51:30 +0300

    Создан интерфейс класса List

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -1,119 +1,65 @@
 function List() {
-    var _size = 0;
-    var _items = [];
-    var _stack = null;
-    var _head = null;
-    var DATA = 0;
-    var NEXT = 1;
-    var _getEmptyCell = function () {
-        var index;
-        if (_stack !== null) {
-            if (_stack.getSize() === 0) {
-                _items.push([null, null]);
-                index = _items.length - 1;
-            } else {
-                index = _stack.pop();
-            }
-        } else {
-            _items.push([null, null]);
-            index = _items.length - 1;
-        }
-        return index;
+    this._size = 0;
+    this._SizeStack = 0;
+    this._data = [];
+    this._nextAdress = [];
+    this._head = null;
+    this._tail = null;
+    this._headStack = null;
+    this._tailStack = null;
+    //Алгоритмы
+    this._getEmptyCellFunction;
+    this._deleteItemFunction;
+    this._pushFunction;
+    this._popFunction;
+    this._insertFunction;
+    this._deleteItemFunction;
+    this._unshiftFunction;
+
+    this._getEmptyCell = function () {
+        this._getEmptyCellFunction();
     };
-    var _deleteItem = function (index) {
-        if (index === _items.length - 1) {
-            _items.pop();
-        } else {
-            _items[index][DATA] = null;
-            _items[index][NEXT] = null;
-            if (_stack === null) {
-                _stack = new List();
-            }
-            _stack.push(index);
-        }
+    this._deleteItem = function (index) {
+        this._deleteItemFunction(index);
     };
     this.getSize = function () {
-        return _size;
+        return this._size;
     };
     this.push = function (x) {
-        var temp = _head;
-        _head = _getEmptyCell();
-        _items[_head][NEXT] = temp;
-        _items[_head][DATA] = x;
-        _size += 1;
+        this._pushFunction(x);
     };
     this.pop = function () {
-        var x;
-        if (_size > 0) {
-            x = _items[_head][DATA];
-            var tempHead = _head;
-            _head = _items[_head][NEXT];
-            _deleteItem(tempHead);
-            _size -= 1;
-        } else {
-            x = undefined;
-        }
-        return x;
+        return this._popFunction();
     };
     this.insert = function (index, x) {
-        var temp = _head;
-        var prev;
-        var i = _size - 1;
-        var isStart = index === 0;
-        var isMiddle = (_head !== null) && (index !== _head);
-        if (isStart) {
-            this.unshift(x);
-        } else {
-            if (isMiddle) {
-                while (i !== index) {
-                    prev = temp;
-                    temp = _items[temp][NEXT];
-                    i -= 1;
-                    if (i < 0)
-                        return undefined;
-                }
-                var cell = _getEmptyCell();
-                _items[cell][DATA] = x;
-                _items[cell][NEXT] = temp;
-                _items[prev][NEXT] = cell;
-                _size += 1;
-            } else {
-                this.push(x);
-            }
-        }
+        this._insertFunction(index, x);
     };
     this.delete = function (index) {
-        var temp = _head;
-        var prev;
-        var i = _size - 1;
-        var x;
-        if (index !== _head) {
-            while (i !== index) {
-                prev = temp;
-                temp = _items[temp][NEXT];
-                i -= 1;
-                if (i < 0)
-                    return undefined;
-            }
-            _items[prev][NEXT] = _items[temp][NEXT];
-            x = _items[temp][DATA];
-            _deleteItem(temp);
-        } else {
-            x = this.pop();
-        }
-        _size -= 1;
-        return x;
+        this._deleteItemFunction(index);
     };
     this.unshift = function (x) {
-        var _temp = _head;
-        while (_items[_temp][NEXT] !== null) {
-            _temp = _items[_temp][NEXT];
-        }
-        var index = _getEmptyCell();
-        _items[index][DATA] = x;
-        _items[index][NEXT] = null;
-        _items[_temp][NEXT] = index;
-        _size += 1;
+        this._unshiftFunction(x);
+    };
 
+    this.setGetEmptyCell = function (func) {
+        this._getEmptyCellFunction = func;
+    };
+    this.setDeleteItem = function (func) {
+        this._deleteItemFunction = func;
+    };
+    this.setPush = function (func) {
+        this._pushFunction = func;
+    };
+    this.setPop = function (func) {
+        this._popFunction = func;
+    };
+    this.setInsert = function (func) {
+        this._insertFunction = func;
+    };
+    this.setDelete = function (func) {
+        this._deleteItemFunction = func;
+    };
+    this.setUnshift = function (func) {
+        this._deleteItemFunction = func;
     };
 }
