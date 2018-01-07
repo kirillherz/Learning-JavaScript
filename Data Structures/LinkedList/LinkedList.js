109dc87a257d65ff05717dd3fe0002ed301e954f
kirillherz <kirill.herz@gmail.com>
2018-01-07 21:44:31 +0300

    Добавлена функция _deleteItem

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -76,3 +76,19 @@ function _getEmptyCell() {
     }
     return index;
 }
+
+function _deleteItem(index) {
+    console.log(this);
+    if (index === this._data.length - 1) {
+        this._data.pop();
+        this._nextAdress.pop();
+    } else {
+        if (this._headStack === null) {
+            this._headStack = index;
+            this._nextAdress[this._headStack] = null;
+        } else {
+            this._nextAdress[index] = this._headStack;
+            this._headStack = index;
+        }
+    }
+}
