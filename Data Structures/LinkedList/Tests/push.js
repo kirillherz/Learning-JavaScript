370a03224c3f0ab4a69a58e420655c2281154e49
kirillherz <kirill.herz@gmail.com>
2018-01-07 22:13:38 +0300

    Доработан автоматический тест для функции push

--- Data Structures/LinkedList/Tests/push.js
+++ Data Structures/LinkedList/Tests/push.js
@@ -4,6 +4,7 @@ describe("push", function () {
         this._data = [];
         this._head = null;
         this._tail = null;
+        this._size = 0;
         push.call(this, 0);
         assert.equal(this._head, 0);
         assert.equal(this._tail, 0);
@@ -21,5 +22,15 @@ describe("push", function () {
         assert.equal(this._tail, 1);
         assert.equal(this._data[this._tail], 1);
         assert.equal(this._nextAdress[this._tail], null);
+    });
+     it("Считает размер списка", function () {
+        this._nextAdress = [null];
+        this._data = [0];
+        this._head = 0;
+        this._tail = 0;
+        push.call(this, 0);
+        push.call(this, 1);
+        assert.equal(this._size, 2);
+     
     });
 });
