d8b521aec3bc9a55bd6ada01f9e53c45d409e98d
kirillherz <kirill.herz@gmail.com>
2018-01-07 20:38:33 +0300

    Реализована функция _getEmptyCell

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -62,3 +62,17 @@ function List() {
         this._deleteItemFunction = func;
     };
 }
+
+function _getEmptyCell() {
+    var index;
+    if (this._headStack === null) {
+        this._data.push(null);
+        this._nextAdress.push(null);
+        index = this._data.length - 1;
+    } else {
+        index = this._headStack;
+        this._headStack = this._nextAdress[this._headStack];
+        this._sizeStack -= 1;
+    }
+    return index;
+}
