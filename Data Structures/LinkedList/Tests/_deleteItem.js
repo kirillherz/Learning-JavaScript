05d309eeab9e7706ba5a27c6f175d915fe2a856e
kirillherz <kirill.herz@gmail.com>
2018-01-07 22:06:37 +0300

    Выполнено форматирование в файле _deleteItem

--- Data Structures/LinkedList/Tests/_deleteItem.js
+++ Data Structures/LinkedList/Tests/_deleteItem.js
@@ -1,18 +1,16 @@
 describe('_deleteItem', function () {
     it('добавляет элемент в стэк свободных ячееек ', function () {
-                this._sizeStack = 0;
-                this._data = [0, 1, 2, 3, 4];
-                this._nextAdress = [1, 2, 3, 4, null];
-                this._headStack = null;
-                _deleteItem.call(this, 2);
-                assert.equal(this._headStack, 2);
-                assert.equal(this._nextAdress[this._headStack], null);
-                _deleteItem.call(this, 0);
-                assert.equal(this._headStack, 0);
-                assert.equal(this._nextAdress[this._headStack], 2);
-
-
-            });
+        this._sizeStack = 0;
+        this._data = [0, 1, 2, 3, 4];
+        this._nextAdress = [1, 2, 3, 4, null];
+        this._headStack = null;
+        _deleteItem.call(this, 2);
+        assert.equal(this._headStack, 2);
+        assert.equal(this._nextAdress[this._headStack], null);
+        _deleteItem.call(this, 0);
+        assert.equal(this._headStack, 0);
+        assert.equal(this._nextAdress[this._headStack], 2);
+    });
     it('Уменьшает массив data если удаляемый индекс попадает на последний элемент ', function () {
 
         this._data = [0, 1, 2, 3, 4];
