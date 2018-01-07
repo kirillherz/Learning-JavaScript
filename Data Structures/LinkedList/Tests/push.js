4733ae03025f7d315ebb6041194dc611d7496695
kirillherz <kirill.herz@gmail.com>
2018-01-07 22:06:51 +0300

    Добавлен автоматический тест для функции push

---
+++ Data Structures/LinkedList/Tests/push.js
@@ -0,0 +1,25 @@
+describe("push", function () {
+    it("Добавляет элемент в пустой список", function () {
+        this._nextAdress = [];
+        this._data = [];
+        this._head = null;
+        this._tail = null;
+        push.call(this, 0);
+        assert.equal(this._head, 0);
+        assert.equal(this._tail, 0);
+        assert.equal(this._data[this._head], 0);
+        assert.equal(this._nextAdress[this._head], null);
+
+    });
+    it("Добавляет элемент в непустой список", function () {
+        this._nextAdress = [null];
+        this._data = [0];
+        this._head = 0;
+        this._tail = 0;
+        push.call(this, 1);
+        assert.equal(this._head, 0);
+        assert.equal(this._tail, 1);
+        assert.equal(this._data[this._tail], 1);
+        assert.equal(this._nextAdress[this._tails], null);
+    });
+});
