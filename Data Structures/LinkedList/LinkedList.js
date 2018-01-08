c92abf2365294c459944db65120a391d4dcc06c3
kirillherz <kirill.herz@gmail.com>
2018-01-08 17:47:54 +0300

    Добавлена функция push

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -97,3 +97,21 @@ function _deleteItem(index) {
         }
     }
 }
+
+function push(x) {
+    var index;
+    if (this._size === 0) {
+        index = this._getEmptyCell();
+        this._head = index;
+        this._tail = index;
+        this._data[index] = x;
+        this._nextAdress[index] = null;
+    } else {
+        index = this._getEmptyCell();
+        this._nextAdress[this._tail] = index;
+        this._tail = index;
+        this._nextAdress[index] = null;
+        this._data[index] = x;
+    }
+    this._size += 1;
+}
