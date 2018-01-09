98316a40ac8ecb9e2b8d22adb2226b8ec72c2c66
kirillherz <kirill.herz@gmail.com>
2018-01-09 18:32:24 +0300

    Добавлен класс ListBasedArrayBuilderTestPush

--- Data Structures/LinkedList/Builders/ListBuilder.js
+++ Data Structures/LinkedList/Builders/ListBuilder.js
@@ -8,3 +8,26 @@ function ListBuilder() {
     };
 }
 
+function ListBasedArrayBuilderTestPush() {
+    ListBuilder.call(this);
+    var ParentCreateNewList = this.createNewList;
+
+    this.createNewList = function () {
+        ParentCreateNewList.call(this);
+        this._list._nextAdress = [null];
+        this._list._data = [0];
+        this._list._head = 0;
+        this._list._tail = 0;
+        this._list._size = 1;
+        return this;
+    };
+
+    this.buildGetEmptyCellFunction = function () {
+        this._list.setGetEmptyCell(_getEmptyCell);
+        return this;
+    };
+    this.buildPushFunction = function () {
+        this._list.setPush(push);
+        return this;
+    };
+}
