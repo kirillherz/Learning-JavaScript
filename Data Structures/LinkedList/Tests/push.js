dc3dba3b5e765499648123f9ddbf984643109ca5
kirillherz <kirill.herz@gmail.com>
2018-01-10 21:10:56 +0300

    Исправлен автоматический тест для добавления в пустой список

--- Data Structures/LinkedList/Tests/push.js
+++ Data Structures/LinkedList/Tests/push.js
@@ -1,11 +1,18 @@
 describe("push", function () {
-    var listBuilder = new ListBasedArrayBuilder();
-    var list = listBuilder
-            .createNewList()
-            .buildGetEmptyCellFunction()
-            .buildPushFunction()
-            .getList();
+
     it("Добавляет элемент в пустой список", function () {
+        var list = {_data: [],
+            _nextAdress: [],
+            _head: null,
+            _tail: null,
+
+            _size: 0,
+            _getEmptyCell: function () {
+                this._data.push(null);
+                this._nextAdress.push(null);
+                return 0;
+            },
+            push: push};
         list.push(0);
         assert.equal(list._head, 0);
         assert.equal(list._tail, 0);
