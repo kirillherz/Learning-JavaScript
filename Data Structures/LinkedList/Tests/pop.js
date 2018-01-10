b00ca0100ed93e1b2e8d8de773704b31e6451765
kirillherz <kirill.herz@gmail.com>
2018-01-10 23:02:04 +0300

    Добавлен автоматический тест для функции pop
    Добавлен тест проверяющий правильно ли считает размер списка функция pop

--- Data Structures/LinkedList/Tests/pop.js
+++ Data Structures/LinkedList/Tests/pop.js
@@ -16,6 +16,20 @@ describe("pop", function () {
         assert.equal(list._tail, "При пустом списке указатель tail не равен null");
 
     });
+    it("Учитывает размер списка", function () {
+        var list = {_data: [0, null, 1, null, 2, 3, 4],
+            _nextAdress: [2, null, 4, null, 5, 6, null],
+            _head: null,
+            _tail: 6,
+            _size: 4,
+            _deleteItem: function (index) {},
+            pop: pop};
+        var index = 4;
+        while (list._size > 0) {
+            list.pop();
+            index -= 1;
+            assert.equal(list._size, index, "Размер списка не соответствует действительности");
+        }
     });
 }
 );
