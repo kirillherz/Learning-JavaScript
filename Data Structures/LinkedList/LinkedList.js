42f15bc027dc39d60957798d43f7c987dce1d229
kirillherz <kirill.herz@gmail.com>
2018-01-11 18:42:50 +0300

    Добавлена функция pop

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -138,11 +138,27 @@ function push(x) {
     }
     this._size += 1;
 }
-var data = [0,1,2,3,4];
+
 function pop() {
     var x;
-    x = data.pop();
-    this._size -= 1;
+    if (this._size === 1) {
+        x = this._data[this._head];
+        this._deleteItem(this._head);
+        this._head = null;
+        this._tail = null;
+        this._size -= 1;
+    } else {
+        var pHead = this._head;
+        var pPrev = null;
+        while (this._nextAdress[pHead] !== null) {
+            pPrev = pHead;
+            pHead = this._nextAdress[pHead];
+        }
+        var x = this._data[pHead];
+        this._nextAdress[pPrev] = null;
+        this._deleteItem(pHead);
+        this._size -= 1;
+        this._tail = pPrev;
+    }
     return x;
-    
 }
