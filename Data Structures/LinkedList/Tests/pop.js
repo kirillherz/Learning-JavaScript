b1c5a641b444d3c4abf5d4ddafe53156113bb909
kirillherz <kirill.herz@gmail.com>
2018-01-11 18:42:06 +0300

    Исправлен автоматический тест для функции pop
    Исправлен тест проверяющий правильность возвращения последнего элемента

--- Data Structures/LinkedList/Tests/pop.js
+++ Data Structures/LinkedList/Tests/pop.js
@@ -2,9 +2,9 @@ describe("pop", function () {
     it("Возвращает и удаляет последний элемент из списка", function () {
         var list = {_data: [0, null, 1, null, 2, 3, 4],
             _nextAdress: [2, null, 4, null, 5, 6, null],
-            _head: null,
+            _head: 0,
             _tail: 6,
-            _size: 4,
+            _size: 5,
             _deleteItem: function (index) {},
             pop: pop};
         var index = 4;
@@ -12,8 +12,8 @@ describe("pop", function () {
             assert.equal(list.pop(), index, "Возвращаемый элемент не соответствует ожидаемому");
             index -= 1;
         }
-        assert.equal(list._head, "При пустом списке указатель head не равен null");
-        assert.equal(list._tail, "При пустом списке указатель tail не равен null");
+        assert.equal(list._head,null, "При пустом списке указатель head не равен null");
+        assert.equal(list._tail,null, "При пустом списке указатель tail не равен null");
 
     });
     it("Учитывает размер списка", function () {
