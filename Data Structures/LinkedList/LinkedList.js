737fc5da086e3c9179a42e6687c324072d530177
kirillherz <kirill.herz@gmail.com>
2018-01-12 13:29:11 +0300

    Добавлена функция unshift

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -163,3 +163,17 @@ function pop() {
     }
     return x;
 }
+
+function unshift(x) {
+    var index = this._getEmptyCell();
+    this._data[index] = x;
+    if (this._size === 0) {
+        this._head = index;
+        this._tail = index;
+        this._nextAdress[index] = null;
+    } else {
+        this._nextAdress[index] = this._head;
+        this._head = index;
+    }
+    this._size += 1;
+}
