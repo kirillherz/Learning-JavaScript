7f86da323d068b869254d80898b84dfc7125b347
kirillherz <kirill.herz@gmail.com>
2018-01-12 15:15:03 +0300

    Добавлена функция shift

--- Data Structures/LinkedList/LinkedList.js
+++ Data Structures/LinkedList/LinkedList.js
@@ -177,3 +177,15 @@ function unshift(x) {
     }
     this._size += 1;
 }
+
+function shift() {
+    var x = this._data[this._head];
+    var temp = this._head;
+    this._deleteItem(this._head);
+    this._head = this._nextAdress[temp];
+    if (this._head === null) {
+        this._tail = null;
+    }
+    this._size -= 1;
+    return x;
+}
