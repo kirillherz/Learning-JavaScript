1ebf3e805502db4f8e37304d30ea93a7616eeed0
kirillherz <kirill.herz@gmail.com>
2018-01-12 15:14:31 +0300

    В тест для метода shift добавлена проверка
    Добавлена проверка на возвращение элемента из начала списка с 1 элементом

--- Data Structures/LinkedList/Tests/shift.js
+++ Data Structures/LinkedList/Tests/shift.js
@@ -14,4 +14,18 @@ describe("shift", function () {
         assert.equal(list._tail, 4, "tail не указывает на хвост");
         assert.equal(list._nextAdress[list._head], 2, "Следующий адрес не равен ожидаемому");
     });
+    it("Удаляет элемент из начала списка, если в списке 1 элемент", function () {
+        var list = {_data: [0],
+            _nextAdress: [null],
+            _head: 0,
+            _tail: 0,
+            _size: 1,
+            _deleteItem: function () {
+            },
+            shift: shift};
+
+        assert.equal(list.shift(), 0, "Неверно добавляет значение");
+        assert.equal(list._head, null, "head не указывает на голову");
+        assert.equal(list._tail, null, "tail не указывает на хвост");
+    });
 });
