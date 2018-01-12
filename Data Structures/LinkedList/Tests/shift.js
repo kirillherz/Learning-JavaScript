1443a503c994c256cd57a1930bd585a72ed82a71
kirillherz <kirill.herz@gmail.com>
2018-01-12 15:14:05 +0300

    В тест для метода shift добавлена проверка
    Добавлена проверка на правильное удаление из начала непустого списка

--- Data Structures/LinkedList/Tests/shift.js
+++ Data Structures/LinkedList/Tests/shift.js
@@ -1,2 +1,17 @@
 describe("shift", function () {
+    it("Удаляет элемент из начала списка", function () {
+        var list = {_data: [0, 1, 2, 3, 4],
+            _nextAdress: [1, 2, 3, 4, null],
+            _head: 0,
+            _tail: 4,
+            _size: 5,
+            _deleteItem: function () {
+            },
+            shift: shift};
+
+        assert.equal(list.shift(), 0, "Неверно  значение");
+        assert.equal(list._head, 1, "head не указывает на голову");
+        assert.equal(list._tail, 4, "tail не указывает на хвост");
+        assert.equal(list._nextAdress[list._head], 2, "Следующий адрес не равен ожидаемому");
+    });
 });
