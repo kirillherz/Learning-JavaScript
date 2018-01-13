de804c9c80afe1baddfb1807cab7c235f2b1bb21
kirillherz <kirill.herz@gmail.com>
2018-01-13 19:51:49 +0300

    Проверка добавления в середину списка

--- Data Structures/LinkedList/Tests/insert.js
+++ Data Structures/LinkedList/Tests/insert.js
@@ -54,4 +54,25 @@ describe("insert", function () {
         assert.equal(list._data[list._tail], 1);
         assert.equal(list._nextAdress[list._tail], null);
     });
+    it("Добавляет элемент в середину", function () {
+        console.log(list);
+        var list = {_data: [0, 1, 2, 3],
+            _nextAdress: [1, 2, 3, null],
+            _head: 0,
+            _tail: 0,
+            _size: 1,
+            _getEmptyCell: function () {
+                this._data.push(null);
+                this._nextAdress.push(null);
+                return 4;
+            },
+            insert: insert};
+        list.insert(2, 22);
+        assert.equal(list._data[4] = 22);
+        assert.equal(list._tail, 3);
+        assert.equal(list._head, 0);
+        assert.equal(list._nextAdress[1], 4);
+        assert.equal(list._nextAdress[4], 2);
+    });
+
 });
