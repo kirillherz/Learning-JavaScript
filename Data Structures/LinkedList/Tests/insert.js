a016ee28707192404d3c451fdda5b7282b38064e
kirillherz <kirill.herz@gmail.com>
2018-01-13 19:51:31 +0300

    Проверка добавления в конец списка

--- Data Structures/LinkedList/Tests/insert.js
+++ Data Structures/LinkedList/Tests/insert.js
@@ -35,4 +35,23 @@ describe("insert", function () {
         assert.equal(list._tail, 0, "tail не указывает на хвост");
         assert.equal(list._nextAdress[0], null, "Следующий адрес не равен null");
     });
+    it("Добавляет элемент в конец", function () {
+        console.log(list);
+        var list = {_data: [0],
+            _nextAdress: [null],
+            _head: 0,
+            _tail: 0,
+            _size: 1,
+            _getEmptyCell: function () {
+                this._data.push(null);
+                this._nextAdress.push(null);
+                return 1;
+            },
+            insert: insert};
+        list.insert(0, 1);
+        assert.equal(list._head, 0);
+        assert.equal(list._tail, 1);
+        assert.equal(list._data[list._tail], 1);
+        assert.equal(list._nextAdress[list._tail], null);
+    });
 });
