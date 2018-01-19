d4ac8483bbec1c7bbcae730fff3e85f621a62772
kirillherz <kirill.herz@gmail.com>
2018-01-19 20:27:11 +0300

    Добавлена функция sort

--- Data Structures/Arrays/Arrays.js
+++ Data Structures/Arrays/Arrays.js
@@ -22,3 +22,38 @@ function reverse(arr) {
     }
 }
 Array.prototype.reverse = reverse;
+function sort() {
+    var partition = function (arr, start, end) {
+        var pivot = arr[Math.floor(Math.random() * (end - start + 1)) + start];
+        var l = start;
+        var r = end;
+        while (l <= r) {
+            while (arr[l] < pivot)
+                l++;
+            while (arr[r] > pivot)
+                r--;
+            if (l <= r) {
+                swap(arr, l, r);
+                l++;
+                r--;
+            }
+        }
+        return l;
+    };
+    var swap = function (arr, a, b) {
+        var temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    };
+    var quickSort = function (items, left, right) {
+        var index = partition(items, left, right);
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+    };
+    quickSort(this, 0, this.length - 1);
+}
+Array.prototype.sort = sort;
