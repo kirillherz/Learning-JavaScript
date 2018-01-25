85b35b9ed50687fec6041cf8ea70913c9a4849de
kirillherz <kirill.herz@gmail.com>
2018-01-25 12:53:29 +0300

    Добавлен класс Rabbit

--- Objects/objects.js
+++ Objects/objects.js
@@ -6,3 +6,24 @@ Animal.prototype.run = function () {
     console.log(this.runMsg);
 };
 
+function Rabbit() {
+    Animal.call(this);
+    console.log("Rabbit");
+    this.jumpMsg = "jump";
+}
+//Rabbit.prototype = Animal.prototype; Не подходит т.к например
+//Rabbit.prototype.run и Animal.prototype.run буду ссылаться на одну область
+//При вызове родительской функции run произойдет бесконечная рекурсия
+//Rabbit.prototype = new Animal(); Не подходит т.к конструктор Animal может
+//иметь побочный эффект 
+Rabbit.prototype = Object.create(Animal.prototype);
+
+Rabbit.prototype.jump = function () {
+    console.log(this.jumpMsg);
+};
+
+Rabbit.prototype.run = function () {
+    Animal.prototype.run.call(this);
+    console.log("rabbit");
+};
+
