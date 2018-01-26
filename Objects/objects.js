815e2f5a361c1a4cedf7f956dc6175f1fd5012c9
kirillherz <kirill.herz@gmail.com>
2018-01-26 14:47:35 +0300

    Добавлена функция repeatify

--- Objects/objects.js
+++ Objects/objects.js
@@ -91,3 +91,12 @@ console.log(b);
 //Что будет написано на консоли?
 //ответ 5
 
+
+String.prototype.repeatify = String.prototype.repeatify || function (times) {
+    var str = "";
+    for (var i = 0; i !== times; i++) {
+        str += this;
+    }
+    return str;
+};
+console.log('hello'.repeatify(3));
